c7f21739bd914df41d25161f4e0f630e36727a7a
ashleyy808 <70113941+ashleyy808@users.noreply.github.com>
2021-02-03 21:36:27 -0500

    Update WorldWeatherApi.js

--- src/components/WorldWeatherApi.js
+++ src/components/WorldWeatherApi.js
@@ -1,4 +1,4 @@
-const API_KEY= 'https://api.worldweatheronline.com/premium/v1/marine.ashx?key=694d1afb5f284f19be105900212001&format=json&tide=yes&q=';
+const API_KEY= 'https://api.worldweatheronline.com/premium/v1/marine.ashx?key=xxx&format=json&tide=yes&q=';
 
 
 export default API_KEY;
